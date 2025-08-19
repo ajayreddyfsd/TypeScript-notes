29b2d6bc7e7c7d77cb5ff9a7752e495eb61c24ad
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-08-19 16:56:54 -0400

    basics complete

--- Basics/functions_returning_promise_with_Ttype.tsx
+++ Basics/functions_returning_promise_with_Ttype.tsx
@@ -1,3 +1,8 @@
+//! getData is an async function with a backpack
+//! which takes in string as input and returns promise as output
+//! this async function also comes with a backpack
+//! the type stored in the backpack is what the type of "the value resolved by the promise needs to be" 
+
 export const getData = async <T>(url: string): Promise<T> => {
     const response = await fetch(url);
     return await response.json();
